39d2db5614884961c4c5bd8aa0283c203f5db03e
Tim Wilhoit <tilimil@hotmail.com>
2014-10-06 12:53:44 -0500

    update build script to fix syntax issue

--- build_db.sh
+++ build_db.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#
 # 
 sudo sqlite3 /home/pi/templog.db 'DROP TABLE temps;'
 sudo sqlite3 /home/pi/templog.db 'CREATE TABLE temps(timestamp timestamp default (strftime('%s', 'now')), sensnum numeric, temp numeric);' 
